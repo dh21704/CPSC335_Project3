e09fca10d93a16d51ab98fc90608533cf066f24d
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2025-11-14 19:56:07 -0800

    Update algo2.py

--- algo2.py
+++ algo2.py
@@ -1,1 +1,33 @@
-print('herl;kj')
+import random
+
+def algo2(l):
+    
+    if len(l) <= 1:
+        return l
+    
+    #select a pivot randomly from the list 
+    num = random.randint(1, len(l)-1)
+    pivot = l[num]
+    
+    
+    less_than = []
+    greater_than = []
+    equal_than = []
+    
+    for i in l:
+        if i < pivot:
+            less_than.append(i)
+        elif i > pivot:
+            greater_than.append(i)
+        else:
+            equal_than.append(i)
+    
+    return algo2(less_than) + [pivot] + algo2(greater_than)
+    
+    
+    
+    
+    
+x = [5, 3, 8, 4, 2, 7, 1, 10]
+re = algo2(x)
+print(re)
